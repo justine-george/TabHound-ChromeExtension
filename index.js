b436a4171db28332dd8e3293563974223179f12f
justinegeo96 <justinegeo96@gmail.com>
2023-02-19 02:01:09 -0600

    Adds icon, uses array object now

--- index.js
+++ index.js
@@ -1,13 +1,21 @@
 const saveCurrTabBtn = document.getElementById("save-currtab-btn")
 const saveAllTabsBtn = document.getElementById("save-alltabs-btn")
 const deleteBtn = document.getElementById("delete-all-btn")
-
-const ulTablistEl = document.getElementById("ul-tablist-element")
-
 // get backup-google-btn element
 const backupBtn = document.getElementById("backup-google-btn")
 
-// save format: url + space + title
+const ulTablistEl = document.getElementById("ul-tablist-element")
+
+/*
+const savedLinks = [
+    {
+        id: "8f81f04d-ef4a-4e12-8a38-2d2c57a9d446",
+        url: "https://www.google.com/",
+        title: "Google",
+        iconUrl: "https://www.google.com/images/branding/googlelogo/2x/googlelogo_color_272x92dp.png"
+    },
+];
+*/
 let savedLinks = []
 const leadsFromLocalStorage = JSON.parse(localStorage.getItem("savedLinks"))
 if (leadsFromLocalStorage) {
@@ -17,21 +25,21 @@ if (leadsFromLocalStorage) {
 
 function render(savedLinks) {
     let listItems = ""
-    for (let i = 0; i < savedLinks.length; i++) {
-        const [url, ...rem] = savedLinks[i].split(" ")
-        const title = rem.join(" ")
-        let displayTitle = title
-        if (title.length > 50)
-            displayTitle = displayTitle.substr(0, 48) + "..."
+    for (const savedLink of savedLinks) {
+        // const [url, ...rem] = savedLinks[i].split(" ")
+        // const title = rem.join(" ")
+        const iconUrl = (savedLink.iconUrl === "") ? "./white_icon.png" : savedLink.iconUrl
+        let displayTitle = savedLink.title
+        if (savedLink.title.length > 60)
+            displayTitle = displayTitle.substr(0, 60) + "..."
         listItems += `
-            <div class="tab-row">
-                <li>
-                    <a target='_blank' href='${url}'>
-                        ${displayTitle}
-                    </a>
-                </li>
+            <div class="tab-row-container">
+                <a target='_blank' href='${savedLink.url}' class="tab-row">
+                    <img class="tab-icon" src="${iconUrl}">
+                    <li>${displayTitle}</li>
+                </a>
                 <div class="close-btn">
-                    <span class="material-symbols-outlined delete-row-btn" data-id="${url} ${title}">close</span>
+                    <span class="material-symbols-outlined delete-row-btn" data-id="${savedLink.id}">close</span>
                 </div>
             </div>
         `
@@ -39,43 +47,60 @@ function render(savedLinks) {
     ulTablistEl.innerHTML = listItems
     const closeBtns = document.querySelectorAll(".delete-row-btn")
     closeBtns.forEach(closeBtn => {
-        closeBtn.addEventListener("click", function(val) {
+        closeBtn.addEventListener("click", function (val) {
             deleteFromSavedLinks(val.target.dataset.id)
-            localStorage.setItem("savedLinks", JSON.stringify(savedLinks) )
+            localStorage.setItem("savedLinks", JSON.stringify(savedLinks))
             render(savedLinks)
         })
     })
 }
 
-function deleteFromSavedLinks(value) {
-    const index = savedLinks.indexOf(value)
-    if (index > -1)
-        savedLinks.splice(index, 1) // remove one element starting from index
+function deleteFromSavedLinks(uuid) {
+    const index = savedLinks.findIndex(link => link.id === uuid);
+    if (index !== -1) {
+        savedLinks.splice(index, 1);
+    }
 }
 
-saveCurrTabBtn.addEventListener("click", function(){    
-    chrome.tabs.query({active: true, currentWindow: true}, function(tabs){
-        const iconUrl = tabs[0].favIconUrl
-
-        if (!savedLinks.includes(tabs[0].url + " " + tabs[0].title))
-            savedLinks.unshift(tabs[0].url + " " + tabs[0].title)
-        localStorage.setItem("savedLinks", JSON.stringify(savedLinks) )
-        render(savedLinks)
-    })
-})
-
-saveAllTabsBtn.addEventListener("click", function(){    
-    chrome.tabs.query({currentWindow: true}, function(tabs){
-        for (let tab of tabs) {
-            if (!savedLinks.includes(tab.url + " " + tab.title))
-                savedLinks.unshift(tab.url + " " + tab.title)
+saveCurrTabBtn.addEventListener("click", function () {
+    chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+        const existingLink = savedLinks.find(item => item.url === tabs[0].url && item.title === tabs[0].title)
+        if (!existingLink) {
+            addToSavedLinks(
+                self.crypto.randomUUID(),
+                tabs[0].url,
+                tabs[0].title,
+                tabs[0].favIconUrl
+            )
         }
-        localStorage.setItem("savedLinks", JSON.stringify(savedLinks))   
+        localStorage.setItem("savedLinks", JSON.stringify(savedLinks))
         render(savedLinks)
     })
 })
 
-deleteBtn.addEventListener("dblclick", function() {
+saveAllTabsBtn.addEventListener("click", function () {
+    chrome.tabs.query({ currentWindow: true }, function (tabs) {
+        for (let tab of tabs) {
+            let existingLink = savedLinks.find(item => item.url === tab.url && item.title === tab.title)
+            if (!existingLink) {
+                addToSavedLinks(
+                    self.crypto.randomUUID(),
+                    tab.url,
+                    tab.title,
+                    tab.favIconUrl
+                )
+            }
+        }
+        localStorage.setItem("savedLinks", JSON.stringify(savedLinks))
+        render(savedLinks)
+    })
+})
+
+function addToSavedLinks(id, url, title, iconUrl) {
+    savedLinks.unshift({ id, url, title, iconUrl })
+}
+
+deleteBtn.addEventListener("dblclick", function () {
     localStorage.clear()
     savedLinks = []
     render(savedLinks)
